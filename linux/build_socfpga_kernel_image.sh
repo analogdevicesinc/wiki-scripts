99ca107bf39e25bd0d6c1661947f25fbae1d25b0
Alexandru Ardelean <alexandru.ardelean@analog.com>
2019-02-28 16:02:58 +0200

    linux: add socfpga build rules
    Signed-off-by: Alexandru Ardelean <alexandru.ardelean@analog.com>

---
+++ linux/build_socfpga_kernel_image.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+set -e
+
+LINUX_DIR="${1:-linux-adi}"
+DTFILE="$2"
+CROSS_COMPILE="$3"
+
+HOST=${HOST:-x86_64}
+
+DEFCONFIG=socfpga_adi_defconfig
+GCC_ARCH=arm-linux-gnueabi
+IMG_NAME="zImage"
+ARCH=arm
+DTDEFAULT=socfpga_arria10_socdk_sdmmc.dts
+
+[ -n "$NUM_JOBS" ] || NUM_JOBS=5
+
+LINARO_GCC_VERSION="5.5.0-2017.10"
+
+get_linaro_link() {
+	local ver="$1"
+	local gcc_dir="${ver:0:3}-${ver:(-7)}"
+	echo "https://releases.linaro.org/components/toolchain/binaries/$gcc_dir/$GCC_ARCH/$GCC_TAR"
+}
+
+# if CROSS_COMPILE hasn't been specified, go with Linaro's
+[ -n "$CROSS_COMPILE" ] || {
+	# set Linaro GCC
+	GCC_DIR=gcc-linaro-${LINARO_GCC_VERSION}-${HOST}_${GCC_ARCH}
+	GCC_TAR=$GCC_DIR.tar.xz
+	if [ ! -d "$GCC_DIR" ] && [ ! -e "$GCC_TAR" ] ; then
+		wget "$(get_linaro_link "$LINARO_GCC_VERSION")"
+	fi
+	if [ ! -d "$GCC_DIR" ] ; then
+		tar -xvf $GCC_TAR || {
+			echo "'$GCC_TAR' seems invalid ; remove it and re-download it"
+			exit 1
+		}
+	fi
+	CROSS_COMPILE=$(pwd)/$GCC_DIR/bin/${GCC_ARCH}-
+}
+
+# Get ADI Linux if not downloaded
+# We won't do any `git pull` to update the tree, users can choose to do that manually
+[ -d "$LINUX_DIR" ] || \
+	git clone https://github.com/analogdevicesinc/linux.git "$LINUX_DIR"
+
+export ARCH
+export CROSS_COMPILE
+
+pushd "$LINUX_DIR"
+
+make $DEFCONFIG
+
+make -j$NUM_JOBS $IMG_NAME UIMAGE_LOADADDR=0x8000
+
+if [ -z "$DTFILE" ] ; then
+	echo
+	echo "No DTFILE file specified ; using default '$DTDEFAULT'"
+	DTFILE=$DTDEFAULT
+fi
+
+make $DTFILE
+
+popd 1> /dev/null
+
+cp -f $LINUX_DIR/arch/$ARCH/boot/$IMG_NAME .
+cp -f $LINUX_DIR/arch/$ARCH/boot/dts/$DTFILE .
+
+echo "Exported files: $IMG_NAME, $DTFILE"
+
