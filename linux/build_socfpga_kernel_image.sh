f80394220d43120cd91b9b46743515fad31a7256
Alexandru Ardelean <alexandru.ardelean@analog.com>
2019-10-28 11:40:29 +0200

    linux: make sure only the major GCC version is used
    And move the scripts lower, so that we have a value for the CROSS_COMPILE
    env-var.
    
    Signed-off-by: Alexandru Ardelean <alexandru.ardelean@analog.com>

--- linux/build_socfpga_kernel_image.sh
+++ linux/build_socfpga_kernel_image.sh
@@ -13,26 +13,6 @@ IMG_NAME="zImage"
 ARCH=arm
 DTDEFAULT=socfpga_arria10_socdk_sdmmc.dts
 
-# FIXME: remove the line below once Talise & Mykonos APIs
-#        dont't use 1024 bytes on stack
-KCFLAGS="$KCFLAGS -Wno-error=frame-larger-than="
-export KCFLAGS
-
-# FIXME: remove this function once kernel gets upgrade and
-#        GCC doesn't report these warnings anymore
-GCC="${CROSS_COMPILE}gcc"
-if [ "$($GCC -dumpversion)" -ge "8" ]; then
-	KCFLAGS="$KCFLAGS -Wno-error=stringop-truncation"
-	KCFLAGS="$KCFLAGS -Wno-error=packed-not-aligned"
-	KCFLAGS="$KCFLAGS -Wno-error=stringop-overflow= -Wno-error=sizeof-pointer-memaccess"
-	KCFLAGS="$KCFLAGS -Wno-error=missing-attributes"
-fi
-
-if [ "$($GCC -dumpversion)" -ge "9" ]; then
-	KCFLAGS="$KCFLAGS -Wno-error=address-of-packed-member -Wno-error=stringop-truncation"
-fi
-export KCFLAGS
-
 [ -n "$NUM_JOBS" ] || NUM_JOBS=5
 
 GCC_VERSION="8.3-2019.03"
@@ -54,6 +34,26 @@ GCC_VERSION="8.3-2019.03"
 	fi
 	CROSS_COMPILE=$(pwd)/$GCC_DIR/bin/${GCC_ARCH}-
 }
+
+# FIXME: remove the line below once Talise & Mykonos APIs
+#        dont't use 1024 bytes on stack
+KCFLAGS="$KCFLAGS -Wno-error=frame-larger-than="
+export KCFLAGS
+
+# FIXME: remove this function once kernel gets upgrade and
+#        GCC doesn't report these warnings anymore
+GCC="${CROSS_COMPILE}gcc"
+if [ "$($GCC -dumpversion | cut -d. -f1)" -ge "8" ]; then
+	KCFLAGS="$KCFLAGS -Wno-error=stringop-truncation"
+	KCFLAGS="$KCFLAGS -Wno-error=packed-not-aligned"
+	KCFLAGS="$KCFLAGS -Wno-error=stringop-overflow= -Wno-error=sizeof-pointer-memaccess"
+	KCFLAGS="$KCFLAGS -Wno-error=missing-attributes"
+fi
+
+if [ "$($GCC -dumpversion | cut -d. -f1)" -ge "9" ]; then
+	KCFLAGS="$KCFLAGS -Wno-error=address-of-packed-member -Wno-error=stringop-truncation"
+fi
+export KCFLAGS
 
 # Get ADI Linux if not downloaded
 # We won't do any `git pull` to update the tree, users can choose to do that manually
