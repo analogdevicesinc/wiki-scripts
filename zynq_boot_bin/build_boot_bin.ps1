079252467235149f85eaa4031323f1abc16b3c75
Nicu Siderias <nicu.siderias@analog.com>
2025-11-10 13:14:12 +0200

    Enhance argumets usage
    Add posibility to either download the u-boot or use the desire .elf
    
    Handle multiple download with wget to overwrite the file
    
    Add Support for kv260
    
    Signed-off-by: Nicu Siderias <nicu.siderias@analog.com>

--- zynq_boot_bin/build_boot_bin.ps1
+++ zynq_boot_bin/build_boot_bin.ps1
@@ -1,10 +1,11 @@
 ﻿$XSA_FILE=$args[0]
-$OUT_FILE=$args[1]
+$UBOOT_FILE=if ($args.Length -ge 2 -and $args[1]) { $args[1] } else { "download" }
+$OUT_FILE=$args[2]
 $BUILD_DIR='build_boot_bin'
 $OUTPUT_DIR='output_boot_bin'
 
 function usage () {
-	echo "usage:powershell.exe .\build_boot_bin.ps1 system_top.xsa [output-archive]"
+	echo "usage:powershell.exe .\build_boot_bin.ps1 system_top.xsa (u-boot.elf | download) [output-archive]"
 	exit 1
 }
 
@@ -20,42 +21,10 @@ Param (
 
 ### Check command line parameters
 if ($null -eq (echo "$XSA_FILE" | Select-String -Pattern .xsa)) {usage}
-if ($null -eq (echo "$UBOOT_FILE" | Select-String -Pattern .elf | Select-String -Pattern u-boot)) {usage}
 
 if (!(Test-Path $XSA_FILE)) {
     echo "$XSA_FILE not found"
     usage
-}
-
-$patterns = @("zed", "ccfmc_.*", "ccbob_.*", "usrpe31x", "zc702", "zc706", "coraz7s")
-$regex = $patterns -join '|'
-$fullPath = (Get-Item $XSA_FILE).FullName
-$dir = Split-Path $fullPath
-$newName = (Get-Item $XSA_FILE).BaseName + ".zip"
-$renamedFilePath = Join-Path $dir $newName
-$extractLocation = Join-Path $dir "extractedXSA"
-Copy-Item $XSA_FILE $renamedFilePath
-Expand-Archive -Path $renamedFilePath -DestinationPath $extractLocation -Force
-
-# Search inside extracted files
-$line = Get-ChildItem -Path $extractLocation\*.hwh -Recurse -File |
-    Get-Content |
-    Select-String -Pattern "PATH_TO_FILE" |
-	Select-Object -First 1 -ExpandProperty Line
-$carrier = [regex]::Match($line, $regex).Value	
-
-switch -Wildcard ($carrier) {
-    "zed"      { $UBOOT_FILE = "u-boot_zynq_zed.elf" }
-    "ccfmc_*"  { $UBOOT_FILE = "u-boot_zynq_adrv9361.elf" }
-    "ccbob_*"  { $UBOOT_FILE = "u-boot_zynq_adrv9361.elf" }
-    "usrpe31x" { $UBOOT_FILE = "u-boot-usrp-e310.elf" }
-    "zc702"    { $UBOOT_FILE = "u-boot_zynq_zc702.elf" }
-    "zc706"    { $UBOOT_FILE = "u-boot_zynq_zc706.elf" }
-    "coraz7s"  { $UBOOT_FILE = "u-boot_zynq_coraz7.elf" }
-    Default {
-        Write-Host "`n`n!!!!! Undefined carrier name for uboot selection !!!!!`n`n"
-        exit 126
-    }
 }
 
 if (!(Get-Command xsct)) {
@@ -74,10 +43,55 @@ if (-not ($tool_version -match "^20[1-9][0-9]\.[0-9]$")) {
     exit 1
 }
 
-$boot_partition_location = $tool_version -replace "\.", "_r"
+if ($UBOOT_FILE -eq "download") {
+	$patterns = @("zed", "ccfmc_.*", "ccbob_.*", "usrpe31x", "zc702", "zc706", "coraz7s")
+	$regex = $patterns -join '|'
+	$fullPath = (Get-Item $XSA_FILE).FullName
+	$dir = Split-Path $fullPath
+	$newName = (Get-Item $XSA_FILE).BaseName + ".zip"
+	$renamedFilePath = Join-Path $dir $newName
+	$extractLocation = Join-Path $dir "extractedXSA"
+	Copy-Item $XSA_FILE $renamedFilePath
+	Expand-Archive -Path $renamedFilePath -DestinationPath $extractLocation -Force
 
-Write-Host "Downloading $UBOOT_FILE ..."
-Invoke-WebRequest -Uri "https://swdownloads.analog.com/cse/boot_partition_files/uboot/$boot_partition_location/$UBOOT_FILE" -OutFile $UBOOT_FILE
+	# Search inside extracted files
+	$line = Get-ChildItem -Path $extractLocation\*.hwh -Recurse -File |
+	    Get-Content |
+	    Select-String -Pattern "PATH_TO_FILE" |
+		Select-Object -First 1 -ExpandProperty Line
+	$carrier = [regex]::Match($line, $regex).Value	
+
+	switch -Wildcard ($carrier) {
+	    "zed"      { $UBOOT_FILE = "u-boot_zynq_zed.elf" }
+	    { $_ -like "ccfmc_*" -or $_ -like "ccbob_*" }  { $UBOOT_FILE = "u-boot_zynq_adrv9361.elf" }
+	    "usrpe31x" { $UBOOT_FILE = "u-boot-usrp-e310.elf" }
+	    "zc702"    { $UBOOT_FILE = "u-boot_zynq_zc702.elf" }
+	    "zc706"    { $UBOOT_FILE = "u-boot_zynq_zc706.elf" }
+	    "coraz7s"  { $UBOOT_FILE = "u-boot_zynq_coraz7.elf" }
+	    Default {
+	        Write-Host "`n`n!!!!! The specified carrier does not have a downloadable u-boot.elf file !!!!!`n`n"
+	        exit 1
+	    }
+	}
+
+	$boot_partition_location = $tool_version -replace "\.", "_r"
+
+	Write-Host "Downloading $UBOOT_FILE ..."
+	# Remove existing file to ensure clean download
+	if (Test-Path $UBOOT_FILE) {
+		Remove-Item $UBOOT_FILE -Force
+	}
+	Invoke-WebRequest -Uri "https://swdownloads.analog.com/cse/boot_partition_files/uboot/$boot_partition_location/$UBOOT_FILE" -OutFile $UBOOT_FILE
+}
+else {
+	if ($UBOOT_FILE -notmatch "\.elf" -and $UBOOT_FILE -notmatch "uboot" -and $UBOOT_FILE -notmatch "u-boot") {
+        	usage
+    	}
+	if (-not (Test-Path $UBOOT_FILE)) {
+        	Write-Host "$UBOOT_FILE File not found!"
+        	usage
+	}
+}
 
 Remove-Item -Recurse -Force -ErrorAction:SilentlyContinue $BUILD_DIR
 Remove-Item -Recurse -Force -ErrorAction:SilentlyContinue $OUTPUT_DIR
